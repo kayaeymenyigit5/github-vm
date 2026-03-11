75f7b823175f1212c4047a71ff67e8509f5ee8a7
kayaeymenyigit5 <kayaeymenyigit5@gmail.com>
2026-03-11 20:28:46 +0300

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -21,62 +21,13 @@ echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 #install ngrok
-brew install zrok2
-#configure ngrok and start it
-zrok2 enable 6uX9F7fXzauS
-#Downloads
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
-#disable spotlight indexing
-sudo mdutil -i off -a
-#Create new account
-sudo dscl . -create /Users/runneradmin
-sudo dscl . -create /Users/runneradmin UserShell /bin/bash
-sudo dscl . -create /Users/runneradmin RealName Runner_Admin
-sudo dscl . -create /Users/runneradmin UniqueID 1001
-sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
-sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo createhomedir -c -u runneradmin > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership runneradmin
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-#install ngrok
-brew install zrok2
-#configure ngrok and start it
-zrok2 enable 6uX9F7fXzauS
-#Downloads
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
-#disable spotlight indexing
-sudo mdutil -i off -a
-#Create new account
-sudo dscl . -create /Users/runneradmin
-sudo dscl . -create /Users/runneradmin UserShell /bin/bash
-sudo dscl . -create /Users/runneradmin RealName Runner_Admin
-sudo dscl . -create /Users/runneradmin UniqueID 1001
-sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
-sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo createhomedir -c -u runneradmin > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership runneradmin
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-#install ngrok
 brew install --formula tailscale
 sudo brew services start tailscale
 brew install --cask localxpose
+brew tap localtonet/tap
+brew install localtonet
 #configure ngrok and start it
+localtonet --authtoken ug37AeB4xOCZ09MSY6iRUcEdpshvVobwj
 loclx account login ESEjgBVa9vBa4RL1ZENWXms1gOG2Mwy8LqUGcNFV
 loclx tunnel http --to localhost:4040
 sudo tailscale up --authkey=$1 --hostname=macos
